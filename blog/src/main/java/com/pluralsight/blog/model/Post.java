06b4fc55a9f5e810d06464f2edb0d685ebe51846
Nishant3092 <NRAJ5@LIN33001591.corp.capgemini.com>
2022-10-14 16:47:55 +0530

    Learn Rest API

---
+++ blog/src/main/java/com/pluralsight/blog/model/Post.java
@@ -0,0 +1,100 @@
+package com.pluralsight.blog.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Entity
+public class Post {
+
+	@Version
+	private Long version;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Author author;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(min = 4, max = 100)
+	private String title;
+
+	@Column(length = 1000000)
+	@Lob
+	private String body;
+
+	@Temporal(TemporalType.DATE)
+	@CreationTimestamp
+	private Date date;
+
+	public Post() {
+		super();
+	}
+
+	public Post(String title, String body) {// , Author author) {
+		this();
+		this.title = title;
+		this.body = body;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getDateStr() {
+		System.out.println("POST ENTITY  Call...............");
+		DateFormat outputFormatter = new SimpleDateFormat("MM/dd/yyyy");
+		return outputFormatter.format(this.date);
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Post))
+			return false;
+		Post otherPost = (Post) obj;
+		return this.title.equals(otherPost.getTitle()) && this.body.equals(otherPost.getBody());
+	}
+}
